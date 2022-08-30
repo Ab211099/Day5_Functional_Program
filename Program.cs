eca9e001f8802c88dfee3a1716a5ee7809516f19
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-08-30 12:52:14 +0530

    [Abhi]Added: Flip coin program

--- Program.cs
+++ Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello welcome to Basic Core Programs");
+            Console.WriteLine("Enter the program number \n 1.Flip coin");
+            int proramCheck = Convert.ToInt32(Console.ReadLine());
+
+            switch (proramCheck)
+            {
+                case 1:
+                    Flip_Coin.GetFlipCoin();
+                    break;   
+            }
             
         }
     }
