0fed5f388c6afccd722e2840e66463fd6bcf187f
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-08-30 12:32:23 +0530

    Welcome Program

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Basic_core_programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to Basic Core Programs");
+            
+        }
+    }
+}
