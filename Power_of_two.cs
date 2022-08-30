4772f99da27d03bf3b5188800878d577c49e6f46
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-08-30 13:37:36 +0530

    [Abhi]Added: Power Of Two program

---
+++ Power_of_two.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_core_programs
+{
+    internal class Power_of_two
+    {
+        static public void FindPowertwo()
+        {
+            Console.WriteLine("Enter number upto which we want power of two:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n >= 0 && n < 31)
+            {
+                double pow = Math.Pow(2, n);
+                Console.WriteLine("2 power {0} is {1}", n, pow);
+            }
+            else
+            {
+                Console.WriteLine("Entered number not valid");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello welcome to Basic Core Programs");
-            Console.WriteLine("Enter the program number \n 1.Flip coin \n 2.Leap year");
+            Console.WriteLine("Enter the program number \n 1.Flip coin \n 2.Leap year" +
+                "\n 3. Find Power Of Two");
             int proramCheck = Convert.ToInt32(Console.ReadLine());
 
             switch (proramCheck)
@@ -15,6 +16,9 @@
                     break;   
                 case 2:
                     Leap_year.FindleapYear();
+                    break;
+                case 3:
+                    Power_of_two.FindPowertwo();
                     break;
             }
             
