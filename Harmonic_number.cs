da65e979b640abffc2ff072ad961925e4fa1d94a
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-08-30 17:06:26 +0530

    [Abhi]Added: harmonic Number program

---
+++ Harmonic_number.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_core_programs
+{
+    internal class Harmonic_number
+    {
+
+       
+        public static void Number()
+        {
+            int n = 5;
+            double value = 0.0;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                value += 1 / (float)i;
+            }
+            Console.WriteLine("Sum of Series upto {0} terms : {1} ", n, value);
+        }
+    }
+}
